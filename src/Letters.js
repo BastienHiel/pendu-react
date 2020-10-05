44b76a5fe8451c6b4c8f7fc44940c1e84d25a2b3
Bastien H <bastien.hiel@gmail.com>
2020-10-05 17:54:29 +0200

    improved letters display

--- src/Letters.js
+++ src/Letters.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 
 import Letter from './Letter'; 
-
+import './Letters.sass'; 
 
 const Letters = ({ alphabet, onClick }) => {
     const letterList = alphabet.map((letter, i) => <Letter value={letter} onClick={onClick} key={i} />)
-    return <div className='letters'>{letterList}</div>; 
+    return (
+        <div className='letters'>        
+            {letterList}
+        </div>
+    ); 
 }
 
 export default Letters; 
