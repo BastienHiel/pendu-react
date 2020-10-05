928d2bdff9c37bb91b904e74dfef6e0dcc80e03c
Bastien H <bastien.hiel@gmail.com>
2020-10-05 11:59:13 +0200

    setting up App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Letters from './Letters'; 
+
+const Alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
+
 class App extends Component {
+  state = {
+    Alphabet,
+  }
+  computeDisplay(phrase, usedLetters) {  
+    return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
+  }
+
   render() {
+    const { Alphabet } = this.state; 
     return (
-      <p>test</p>
+      <div className='app'>
+        <Letters Alphabet={Alphabet} />
+      </div>      
     )
   }
 }
