f55a80354986ceefb69c980111995bd82a2a4605
Bastien H <bastien.hiel@gmail.com>
2020-10-05 17:53:48 +0200

    improved letters display

--- src/Letter.js
+++ src/Letter.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 import { Button } from 'semantic-ui-react'; 
 
-const Letter = ({ value, onClick }) => <Button primary onClick={onClick}>{value}</Button>
+import './Letter.sass'; 
+
+const Letter = ({ value, onClick }) => <Button primary className='letter' onClick={onClick}>{value}</Button>
 
 export default Letter; 
 
